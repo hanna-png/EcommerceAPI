aa703f9b5533a05fca5cb70e77ea5e5075425692
Hanna Kirieieva <kireevaann2006@gmail.com>
2026-01-21 07:44:47 +0100

    docs: add docstrings

--- ecommerceapi/api/routes/category.py
+++ ecommerceapi/api/routes/category.py
@@ -10,4 +10,7 @@ category_router = APIRouter(prefix="/categories", tags=["categories"])
 
 @category_router.get("", response_model=list[CategoryOut])
 def list_categories(db: Session = Depends(get_db)):
+    """
+    Retrieve a list of all product categories.
+    """
     return get_categories(db)
