79fadf2654502776b54bb13527a6301691d02945
Hanna Kirieieva <kireevaann2006@gmail.com>
2026-01-26 11:23:30 +0100

    test: test products/search endpoint

--- tests/test_products.py
+++ tests/test_products.py
@@ -3,22 +3,21 @@ from tests.factories.category_factory import CategoryFactory
 from tests.factories.product_factory import ProductFactory
 
 
-def test_category_products_returns_only_that_category(
+def test_products_search_returns_correct_products(
     test_client: TestClient,
     category_factory: CategoryFactory,
     product_factory: ProductFactory,
 ):
-    cat = CategoryFactory()
-    ProductFactory.create_batch(3, category=cat)
+    product_factory(name="Book1")
+    product_factory(name="Phone1")
+    product_factory(description="This is a book")
 
-    resp = test_client.get(f"/categories/{cat.id}/products")
+    resp = test_client.get("/products/search?search_text=book")
     assert resp.status_code == 200
     items = resp.json()
-    assert len(items) == 3
+    assert len(items) == 2
     for p in items:
         assert "id" in p
         assert "category_id" in p
-        assert p["category_id"] == cat.id
         assert "name" in p
         assert "price" in p
-        assert "sku" in p
