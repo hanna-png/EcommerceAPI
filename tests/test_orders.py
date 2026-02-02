84a80b9aa02e2779979f7147c2266e06d6d0d9c5
Hanna Kirieieva <kireevaann2006@gmail.com>
2026-02-02 15:27:39 +0100

    refactor: refactor order tests

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -4,18 +4,13 @@ from tests.factories.product_factory import ProductFactory
 from tests.factories.user_factory import UserFactory
 
 
-def test_create_and_list_orders(
+def get_payload(
     test_client: TestClient,
     user_factory: UserFactory,
     product_factory: ProductFactory,
     category_factory: CategoryFactory,
 ):
-    u = user_factory.create()
-
     p = product_factory.create(price=100)
-
-    headers = auth_header(test_client, u.email, "password123")
-
     payload = {
         "payment_method": "card",
         "delivery_method": "courier",
@@ -27,7 +22,20 @@ def test_create_and_list_orders(
         },
         "items": [{"product_id": p.id, "quantity": 2}],
     }
+    return payload
 
+
+def test_create_and_list_orders(
+    test_client: TestClient,
+    user_factory: UserFactory,
+    product_factory: ProductFactory,
+    category_factory: CategoryFactory,
+):
+    u = user_factory.create()
+
+    headers = auth_header(test_client, u.email, "password123")
+
+    payload = get_payload(test_client, user_factory, product_factory, category_factory)
     create = test_client.post("/orders", json=payload, headers=headers)
     assert create.status_code == 201
     created = create.json()
@@ -48,20 +56,10 @@ def test_invalid_payment_method_rejected(
     category_factory: CategoryFactory,
 ):
     u = user_factory.create()
-    p = product_factory.create(price=100)
     headers = auth_header(test_client, u.email, "password123")
-    # invalid enum - no "papypal" available
-    payload = {
-        "payment_method": "paypal",
-        "delivery_method": "courier",
-        "billing": {
-            "full_name": "Jan Kowalski",
-            "city": "Warsaw",
-            "postal_code": "00-001",
-            "country": "PL",
-        },
-        "items": [{"product_id": p.id, "quantity": 1}],
-    }
+    # invalid enum - no "paypal" available
+    payload = get_payload(test_client, user_factory, product_factory, category_factory)
+    payload["payment_method"] = "paypal"
 
     resp = test_client.post("/orders", json=payload, headers=headers)
     assert resp.status_code == 422
@@ -71,6 +69,7 @@ def auth_header(test_client: TestClient, email: str, password: str) -> dict[str,
     resp = test_client.post(
         "/auth/login", data={"username": email, "password": password}
     )
+    """ Helper for checking Bearer token in headers"""
     assert resp.status_code == 200
     token = resp.json()["access_token"]
     return {"Authorization": f"Bearer {token}"}
